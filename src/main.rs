e73213ef6b4552494c72658d5558c2e21facf22c
Emmanuel <olaocha@Emmanuels-MacBook-Air.local>
2026-05-20 13:37:05 +0100

    set up axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Server;
+use ems::routes::app;
+use anyhow::{Context, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let address = "0.0.0.0:3000".parse().expect("Failed to open socket");
+
+    println!("server running on {:?}", &address);
+    Server::bind(&address)
+        .serve(app().into_make_service())
+        .await
+        .context("Failed to start server.")
 }
